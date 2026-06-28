59e7b7b0dc36ec6a6094fe25ee37b8fe196bdf3d
magdalena.kanicka <magdalena.kanicka@gmail.com>
2026-06-28 15:12:07 +0200

    pre-demo changes

--- identity/admin.py
+++ identity/admin.py
@@ -6,7 +6,7 @@ class IdentityNameInline(admin.TabularInline):
     extra = 0
 
 class IdentityAdmin(admin.ModelAdmin):
-    list_display = ('owner', 'gender', 'display_names')
+    list_display = ('id','owner', 'gender', 'display_names', 'is_public')
     inlines = [IdentityNameInline]
 
     def display_names(self, obj):
@@ -16,4 +16,46 @@ class IdentityAdmin(admin.ModelAdmin):
         )
     display_names.short_description = "Names"
     
+    def owner(self, obj):
+        return obj.owner.username
+    
 admin.site.register(Identity, IdentityAdmin)
+
+class NameContextAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(NameContext, NameContextAdmin)
+
+class GenderAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Gender, GenderAdmin)
+
+class RelationshipTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(RelationshipType, RelationshipTypeAdmin)
+
+class IdentityNameAccessAdmin(admin.ModelAdmin):
+    list_display = ('relationship_type', 'name_context')
+
+admin.site.register(IdentityNameAccess, IdentityNameAccessAdmin)
+
+class IdentityRelationshipAdmin(admin.ModelAdmin):
+    list_display = ('consumer', 'relationship_type', 'identity', 'owner')
+    
+    def consumer(self, obj):
+        return obj.consumer.username
+    
+    def owner(self, obj):
+        return obj.identity.owner.username
+
+admin.site.register(IdentityRelationship, IdentityRelationshipAdmin)
+
+class IdentityNameAdmin(admin.ModelAdmin):
+    list_display = ('identity', 'owner', 'name_context', 'name_value', 'is_default')
+    
+    def owner(self, obj):
+        return obj.identity.owner.username
+
+admin.site.register(IdentityName, IdentityNameAdmin)
